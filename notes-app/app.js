ffc1ea3f331341d00544c8bb553f116ed2e56c51
Prashant rawal <Prashantrawal619@gmail.com>
2019-07-19 16:49:11 +0530

    add notes app

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,75 @@
-const fs = require("fs");
+const chalk = require("chalk");
+const yargs = require("yargs");
+const notes = require("./notes.js");
 
-fs.writeFileSync("notes.txt", "this is the Prashant rawal");
-fs.appendFileSync("notes.txt", " i am appended haha");
+// Customize yargs version
+yargs.version("1.1.0");
+
+// Create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string"
+    },
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: "string"
+    }
+  },
+  handler(argv) {
+    notes.addNote(argv.title, argv.body);
+  }
+});
+
+// Create remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string"
+    }
+  },
+  handler(argv) {
+    notes.removeNote(argv.title);
+  }
+});
+
+// Create list command
+yargs.command({
+  command: "list",
+  describe: "List your notes",
+  handler() {
+    notes.listNotes();
+  }
+});
+
+// Create read command
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string"
+    }
+  },
+  handler(argv) {
+    notes.readNote(argv.title);
+  }
+});
+
+yargs.parse();
+
+//while running the node app.js with some arguments like node app.js prashnat then we will have that value in process.argv
+//actually process.argv returns an array with includes the directory where node.js installed ,directory of project ,and third will be our argument which we passes in our cli
+//json.stringfy takes object as argument and output the json while json.parse do the opposite
+//we have used yargs and chalk as the dependencies
