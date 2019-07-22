81db53d105a99f04def0e460c0434aa21197ed8b
Prashant rawal <Prashantrawal619@gmail.com>
2019-07-22 11:30:32 +0530

    add debugging file

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -4,7 +4,7 @@ const chalk = require("chalk");
 const addNote = (title, body) => {
   const notes = loadNotes();
   const duplicateNote = notes.find(note => note.title === title);
-
+  debugger;
   if (!duplicateNote) {
     notes.push({
       title: title,
